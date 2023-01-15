4579e2816e645c3ea21fb33664f54d80d835ddd3
Hayden <asianhaydenxd@gmail.com>
2023-01-15 18:16:17 -0500

    implement calling and indexing

--- parser.lua
+++ parser.lua
@@ -7,7 +7,26 @@ local ctx = {
 
 -- expr ::= atom
 function ctx:parse_expr()
-    return self:parse_atom()
+    return self:parse_call_index()
+end
+
+-- call_index ::= (atom | call_index) ('[' expr ']' | '(' (expr (',' expr)*)? ')')*
+function ctx:parse_call_index()
+    local called = self:parse_atom()
+    while self:match("op", "(") or ctx:match("op", "[") do
+        if self:match("op", "(") then
+            local items = self:parse_comma_brackets("(", ")")
+            self:next()
+            called = { a = "call", called = called, args = items }
+        elseif self:match("op", "[") then
+            self:next()
+            local expression = self:parse_expr()
+            if not self:match("op", "]") then return ctx:err("expected ']'") end
+            self:next()
+            called = { a = "index", indexed = called, arg = expression }
+        end
+    end
+    return called
 end
 
 -- atom ::= IDENTIFIER | NUMBER | STRING | CHAR | parentheses
@@ -68,6 +87,22 @@ function ctx:parse_array()
 end
 
 -- Auxiliary contextual functions
+function ctx:parse_comma_brackets(left, right)
+    if not self:match("op", left) then return self:err("expected '" .. left .. "'") end
+    self:next()
+    local items = {}
+    while not self:match("op", right) do
+        local expression = self:parse_expr()
+        table.insert(items, expression)
+        if self:match("op", ",") then
+            self:next()
+        elseif not self:match("op", right) then
+            return self:err("expected ','")
+        end
+    end
+    return items
+end
+
 function ctx:current_token() return self.tokens[self.index] end
 function ctx:match(label, value) return self:current_token():match(label, value) end
 function ctx:next() self.index = self.index + 1 end
