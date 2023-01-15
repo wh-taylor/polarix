9da1b1a6292d19889318d86569acd8d5b0ec831a
Hayden <asianhaydenxd@gmail.com>
2023-01-14 22:13:00 -0500

    implement character class distinction

--- lexer.lua
+++ lexer.lua
@@ -1,7 +1,23 @@
 local lexer = {}
 
-function lexer.lex()
-    print("lex")
+function lexer.lex(code)
+    local index = 1
+    
+    while index <= #code do
+        local char = code:sub(index, index)
+        
+        if char:match("%d") then
+            print(char .. ": DIGIT")
+        elseif char:match("[ \n\t\r]") then
+            print(char .. ": WHITESPACE")
+        elseif char:match("%p") then
+            print(char .. ": PUNCTUATION")
+        else
+            print(char .. ": LETTER")
+        end
+
+        index = index + 1
+    end
 end
 
 return lexer
