2b2b7862efa073a8fe84765c11c4f990db81f8e2
Hayden <asianhaydenxd@gmail.com>
2023-01-28 02:05:24 -0500

    read file in main

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-mod read_file;
 mod tests;
+mod read_file;
 
 fn main() {
-    
+    let code = read_file::read_file("main.px".to_string());
+
+    if let Err(error) = code {
+        panic!("{}", error);
+    }
 }
