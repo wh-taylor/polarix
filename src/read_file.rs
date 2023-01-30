d1b911dc7f29a801d9e53d1d33eaf55087e9a3a6
Hayden <asianhaydenxd@gmail.com>
2023-01-29 23:53:44 -0500

    remove file reading test

--- src/read_file.rs
+++ src/read_file.rs
@@ -16,17 +16,3 @@ fn get_file_contents(file: &mut File) -> Result<String, Error> {
         Err(error) => Err(error),
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn read_file_test() {
-        let filename = "main.px";
-        match read_file(filename.to_string()) {
-            Ok(text) => assert_eq!(text, "a"),
-            Err(_)   => panic!("file {} not found", filename),
-        }
-    }
-}
