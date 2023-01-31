898c79cde099ca3288e74a2dcecdbcd828748b15
Hayden <asianhaydenxd@gmail.com>
2023-01-31 00:18:34 -0500

    create syntax error collector

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,14 +2,12 @@ use crate::parser::*;
 use crate::nodes::*;
 use crate::values::*;
 
-fn interpret(tree: Result<Expression, SyntaxError>) {
+fn interpret(tree: Result<Expression, SyntaxErrorCollector>) {
     interpret_expression(tree)
 }
 
-fn interpret_expression(tree: Result<Expression, SyntaxError>) {
-    match tree {
-        
-    }
+fn interpret_expression(tree: Result<Expression, SyntaxErrorCollector>) {
+    todo!();
 }
 
 #[cfg(test)]
--- src/parser.rs
+++ src/parser.rs
@@ -3,6 +3,10 @@ use crate::nodes::*;
 use crate::tokens::Token;
 use crate::tokens::{TokenContext, TokenContent::*};
 
+pub struct SyntaxErrorCollector {
+    errors: Vec<SyntaxError>,
+}
+
 pub struct SyntaxError {
     error_type: SyntaxErrorType,
     context: TokenContext,
@@ -11,6 +15,28 @@ pub struct SyntaxError {
 pub enum SyntaxErrorType {
     LexerError(LexerErrorType),
     AtomExpected,
+}
+
+impl SyntaxErrorCollector {
+    fn new() -> SyntaxErrorCollector {
+        SyntaxErrorCollector {
+            errors: Vec::new()
+        }
+    }
+
+    fn add_errors(&mut self, error_collector: SyntaxErrorCollector) {
+        self.errors.extend(error_collector.errors);
+    }
+
+    fn from_error(error_type: SyntaxErrorType, context: TokenContext) -> SyntaxErrorCollector {
+        let mut error_collector = Self::new();
+        error_collector.errors.push(SyntaxError { error_type, context });
+        error_collector
+    }
+
+    fn from_lexer_error(lexer_error: LexerError) -> SyntaxErrorCollector {
+        Self::from_error(SyntaxErrorType::LexerError(lexer_error.error_type), lexer_error.context)
+    }
 }
 
 impl Lexer {
@@ -27,11 +53,11 @@ impl Lexer {
     //     Ok(items)
     // }
 
-    fn parse_expression(&mut self) -> Result<Expression, SyntaxError> {
+    fn parse_expression(&mut self) -> Result<Expression, SyntaxErrorCollector> {
         self.parse_atom()
     }
 
-    fn parse_atom(&mut self) -> Result<Expression, SyntaxError> {
+    fn parse_atom(&mut self) -> Result<Expression, SyntaxErrorCollector> {
         match self.next(ProgramContext::NormalContext) {
             Ok(Some(Token { content: IntToken(int), context: _ })) => {
                 Ok(Expression::IntLiteral { value: int })
@@ -51,14 +77,12 @@ impl Lexer {
             Ok(Some(Token { content: FalseKeyword, context: _ })) => {
                 Ok(Expression::BooleanLiteral { value: false })
             },
-            Ok(_) => Err(SyntaxError {
-                error_type: SyntaxErrorType::AtomExpected,
-                context: self.context.clone(),
-            }),
-            Err(lex_error) => Err(SyntaxError {
-                error_type: SyntaxErrorType::LexerError(lex_error.error_type),
-                context: lex_error.context,
-            }),
+            Ok(_) => {
+                Err(SyntaxErrorCollector::from_error(SyntaxErrorType::AtomExpected, self.context.clone()))
+            },
+            Err(lex_error) => {
+                Err(SyntaxErrorCollector::from_lexer_error(lex_error))
+            },
         }
     }
 }
